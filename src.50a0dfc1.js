68d3749a1499074f22a915bc5c5f7b2b9d6bf537
funz1k <92177445+funz1k@users.noreply.github.com>
2022-01-17 06:38:22 +0000

    Deploying to gh-pages from @ funz1k/teamproject_project14@544151bc89086f057be3967e42b2d8842b79b4a8 🚀

--- src.50a0dfc1.js
+++ src.50a0dfc1.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/teamproject_project14/src.1ba4537d.js.map
+//# sourceMappingURL=/teamproject_project14/src.50a0dfc1.js.map
